7533636aa1a2396b1ceb6c1af53becfa8f371cb4
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-07-14 10:20:31 +1200

    adding api fetch request

--- game-share/src/components/App.js
+++ game-share/src/components/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import styles from '../css/styles.module.css';
 
 export default function App(props) {
+  useEffect(() => {
+    const requestOption = {
+      method: "POST",
+      header: {"user-key": process.env.KEY},
+      body: "fields name; limit 10;"
+    }
+
+    fetch("https://api-v3.igdb.com/games/", requestOption)
+    .then((res) => {
+      console.log(res)
+    })
+    .catch(err => console.log(err));
+  }, [])
+
   return (
     <div>
       <div className={styles.header}>
