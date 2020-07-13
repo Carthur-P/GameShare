c7cc45b59b832d1201d18b79edc76c974f13f9cd
pongp1 <pongp1@student.op.ac.nz>
2020-07-14 08:28:57 +1200

    adding header

--- game-share/src/components/App.js
+++ game-share/src/components/App.js
@@ -4,7 +4,7 @@ import styles from '../css/styles.module.css';
 export default function App(props) {
   return (
     <div>
-      <div className={styles.header, styles.test}>
+      <div className={styles.header}>
         <h1>GameShare</h1>
         <p>Cart</p>
         <p>Sign In</p>
